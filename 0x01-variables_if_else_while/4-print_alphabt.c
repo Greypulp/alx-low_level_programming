1bc21b31f8554c2438afa3573dc219ce1b105072
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-13 16:38:05 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,16 +5,15 @@
  */
 int main(void)
 {
-        char c;
-        
-        c = 'a';
-        
+	char c;
+	
+	c = 'a';
 	while
-                (c <= 'z') {
-			if ((c != 'q' && c != 'e') && (c <= 'z')
-                        putchar(c);
-                        c++;
-                }
-	putchar('\n');
-        return (0);
-}
+		(c <= 'z') {
+			if (( c != 'q' && c != 'e' && c <= 'z')
+					putchar(c);
+					c++;
+					}
+					putchar('\n');
+					return (0);
+					}

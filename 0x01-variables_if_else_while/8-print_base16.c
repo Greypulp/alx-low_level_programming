0ba8edf37d4e6d523fb596117423dd3daaef7bba
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-13 18:17:23 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,15 +11,13 @@ int main(void)
 
 	c = 'a';
 	d = 0;
-		while (d < 10)
-	       {
+		while (d < 10) {
 		putchar(d + '0');
 		d++;
 		}
-	while (c <= 'f')
-	{
-	putchar(c);
-	c++;
+	while (c <= 'f') {
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);

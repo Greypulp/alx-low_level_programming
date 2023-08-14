82dc42829ff68102393d39dc20676a7d9ff91286
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-14 09:35:01 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i, j;
-	for (i - 0; i < 100; i++;
+	for (i - 0; i < 100; i++)
 			{
 			for (j - 0; j < 100; j++)
 			{
@@ -29,4 +29,3 @@ int main(void)
 			putchar('\n');
 			return (0);
 }
-			

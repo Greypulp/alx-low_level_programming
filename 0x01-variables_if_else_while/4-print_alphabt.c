a3cbbbb4554f81dd6c4626395d0510691d68bbc4
DENNIS KURIA <139075719+Greypulp@users.noreply.github.com>
2023-08-14 03:05:32 +0300

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,17 +3,16 @@
 * main - Entry point
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-char = c;
-c = 'a';
-while
-(c <= 'z') {
-if ((c != 'q' && c != 'e') && c <= 'z')
-putchar(c);
-c++;
-}
-putchar('\n');
-return (0);
+  char = c;
+  c = 'a';
+  while
+    (c <= 'z') {
+      if ((c != 'q' && c != 'e') && c <= 'z')
+        putchar(c);
+      c++;
+    }
+  putchar('\n');
+  return (0);
 }

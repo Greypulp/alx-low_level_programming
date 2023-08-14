544cfc3ad882efaa111851b10d5e487fe773ea9e
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-13 17:39:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10)putchar(a + '0');a++;
+	putchar('\n');
+return (0);
+}

41b099040abd85b9b00913bfa9196e6580207f48
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-15 11:56:22 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
-* print_alpahbet_x10 -functions prints 10x in lowercae
+* print_alphabet_x10 - functions prints 10x in lowercae
 * Return: Always 0
 */
 void print_alphabet_x10(void)
 {
 	int i;
 	char j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)

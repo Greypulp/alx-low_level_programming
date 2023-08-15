794cbb4ae7998f8bf70161b60eafe397413495f1
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-15 11:08:36 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
 * main - print lowercase alphabet
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }

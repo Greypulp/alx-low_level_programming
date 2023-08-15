0fac36a285b67a0849bc71143eb4a85a74cd0335
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-15 14:36:58 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
 * print_abs - computes the absolute value of an integer
+* @a: function parameter
 * Return: 0
 **/
 int _abs(int a)
 {
 	if (a < 0)
 		return (-a);
-	else
+	else if (a >= 0)
 	{
 		return(a);
 	}

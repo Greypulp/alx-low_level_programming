2a98f8611f108dab987496afcb7e45d44812a2a2
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-15 14:16:32 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* sign - prints the sign of a character
+* @c: function parameter
+* Return: 1, 0 or -1
+*/
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return('-1');
+	}
+}

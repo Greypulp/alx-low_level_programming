a5fb3c30259dd8f5a385691673e8c468416fabf9
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-16 15:00:25 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+/**
+ * main - function that sum pultipkes of 3 or 5 in numbers below 10
+ * Return: 0
+ */
 
 int main(void)
 {
 	int sum = 0;
 	int i;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;

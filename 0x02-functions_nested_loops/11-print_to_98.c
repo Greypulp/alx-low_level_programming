c1156f4489be96deccf510bbb25ae602fafc365a
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-16 14:31:03 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,15 @@ void print_to_98(int n)
 	if (n >= 0 && n <= 98)
 	{
 		for (i = n; i <= 98; i++)
-				{
-				printf("%d", i);
-				if (i != 98)
-					printf(", ");
-				}
-				printf("\n");
-			}
-	else if (n < 0)
 		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);

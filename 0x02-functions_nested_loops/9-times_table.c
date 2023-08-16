11506f0207eee67e1d0fe6181f9137c6edc5f8e6
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-15 19:55:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * times _table - prints timetable
+ * times_table - prints timetable
  * Return: 0
  */
 
 void times_table(void)
 {
 	int i, j, result;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)

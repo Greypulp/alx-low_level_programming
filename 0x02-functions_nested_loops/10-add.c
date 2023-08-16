3fa0725ff54129190879d88fb1a01d6b9ca44712
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-16 13:39:46 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
 * add - function that adds two numbers
 * @a: parameter 1
 * @b: paramenter 2
 * Return: a + b
 */
-
-int add(int, int)
+int add(int a, int b)
 {
 	return (a + b);
 }

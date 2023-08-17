6b10776f9ac91f83ca3519d5453f0e5155751156
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-08-16 17:08:04 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+
 /**
- * main - prints the first 52 fibonacci numbers
-* Retrun: 0
-*/
+ * main - Prints the first 52 fibonacci numbers
+ * Return: Nothing!
+ */
 
 int main(void)
 
 {
-	unsigned long int term1 = 1;
-	unsigned long int term2 = 2;
-	unsigned long int next;
-	int i;
 
-	printf("%lu, ", term1);
-	for (i = 1; i < 50; i++)
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
 	{
-		printf("%lu", term2);
-		next = term1 + term2;
-		term1 = term2;
-		term2 = next;
-		if (i != 49)
-			printf(", ");
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
+	}
+	++i;
 	}
 	printf("\n");
 	return (0);
+
 }

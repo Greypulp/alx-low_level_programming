d8c87931f1a372d3be03b7f5116878156956d922
DENNIS KURIA <dennis.njoroge@twiga.com>
2023-09-04 14:13:44 +0300

    function that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *   * _strdup - Duplicate a string
+ *     * @str: the string to duplicate
+ *       *
+ *         * Return: the string duplicated
+ *           */
+char *_strdup(char *str)#include "main.h"
+#include <stdlib.h>
+
+/**
+ *   * _strdup - Duplicate a string
+ *     * @str: the string to duplicate
+ *       *
+ *         * Return: the string duplicated
+ *           */
+char *_strdup(char *str)
